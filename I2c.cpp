9f537d147f63082964ce00acc2fa95476091995b
kevinfcc26 <kevinfcc26@gmail.com>
2020-06-06 12:36:46 -0500

    Update

--- I2c.cpp
+++ I2c.cpp
@@ -730,18 +730,16 @@ void Initializing_the_chipset()
     printf("Done\n");
 }
 void SetJson(int Registro, int Sample, float Value){
-    // string Nameobj;
 
     Objregister[Registro].SetConValue(Value);
-    // Nameobj = Objregister[Registro].GetName();
     dataj[std::to_string(Sample)][Objregister[Registro].GetName()] = Value;
+
 }
 // Cargar los valores de Corriente convertidos
-void SetJsonCurrent(int Registro, int Sample)
+void Current(int Registro, int Sample)
 {
     int Temp = 0;
     float Valueobj = 0;
-    string Nameobj;
 
     Objregister[Registro].Read();
     Temp = Objregister[Registro].GetValue();
@@ -750,35 +748,25 @@ void SetJsonCurrent(int Registro, int Sample)
     // Valueobj = 4.2570*pow(10,-6)*Temp  - 0.1855;
     //Valueobj = 4.2550*pow(10,-6)*Temp - 0.1858;
     Valueobj = Temp*(4.2554*pow(10,-6)) - 0.1858;
-    // Valueobj = Temp;
-    // Valueobj = (Temp * (0.03125 / 5320000) * (667 / 75) - (2.8 * pow(10, -4))) / (1.36 * pow(10, -3));
-    Objregister[Registro].SetConValue(Valueobj);
-    Nameobj = Objregister[Registro].GetName();
-    dataj[std::to_string(Sample)][Nameobj] = Valueobj;
+    SetJson(Registro, Sample, Valueobj);
     
     if(Nameobj == "AIRMS"){
-    sumI = sumI + Valueobj;
-    
+        sumI = sumI + Valueobj;
     }
 
-    // cout << Nameobj << endl;
-    // cout << Valueobj << endl;
 }
 // Cargar los valores de Voltaje convertidos
-void SetJsonVol(int Registro, int Sample)
+void Vol(int Registro, int Sample)
 {
     int Temp = 0;
     float Valueobj = 0;
-    string Nameobj;
 
     Objregister[Registro].Read();
     Temp = Objregister[Registro].GetValue();
     dataj[std::to_string(Sample)]["ADCV"]=Temp;
     // Valueobj = Temp * 901 / 10640000;
     Valueobj = Temp * (991150 / 1000) / (10640000);
-    Objregister[Registro].SetConValue(Valueobj);
-    Nameobj = Objregister[Registro].GetName();
-    dataj[std::to_string(Sample)][Nameobj] = Valueobj;
+    SetJson(Registro, Sample, Valueobj);
 
     if(Nameobj == "AVRMS"){
         sumV = sumV + Valueobj;
@@ -788,48 +776,35 @@ void SetJsonVol(int Registro, int Sample)
     // cout << Valueobj << endl;
 }
 
-void SetJsonPower(int Registro, int Sample){
+void Power(int Registro, int Sample){
     int Temp = 0;
     float Valueobj = 0;
-    string Nameobj;
 
     Objregister[Registro].Read();
     Temp = Objregister[Registro].GetValue();
     Valueobj = Temp;
-    Objregister[Registro].SetConValue(Valueobj);
-    Nameobj = Objregister[Registro].GetName();
-    dataj[std::to_string(Sample)][Nameobj] = Valueobj;
-
-    // if(Nameobj == "AVRMS"){
-    //     sumV = sumV + Valueobj;
-    // }
+    SetJson(Registro, Sample, Valueobj);
 }
-void SetJsonTHD(int Registro, int Sample){
+void THD(int Registro, int Sample){
     int Temp = 0;
     float Valueobj = 0;
-    string Nameobj;
 
     Objregister[Registro].Read();
     Temp = Objregister[Registro].GetValue();
     Valueobj = Temp;
-    Objregister[Registro].SetConValue(Valueobj);
-    Nameobj = Objregister[Registro].GetName();
-    dataj[std::to_string(Sample)][Nameobj] = Valueobj;
+    SetJson(Registro, Sample, Valueobj);
 }
-void SetJsonAngle(int Registro, int Sample){
+void Angle(int Registro, int Sample){
     int Temp = 0;   
     float Valueobj = 0;
-    string Nameobj;
 
     Objregister[Registro].Read();
     Temp = Objregister[Registro].GetValue();
-    Valueobj =  acos( Temp * (360*60/250000) ) ;
-    Objregister[Registro].SetConValue(Valueobj);
-    Nameobj = Objregister[Registro].GetName();
-    dataj[std::to_string(Sample)][Nameobj] = Valueobj;
+    Valueobj = Temp ;
+    SetJson(Registro, Sample, Valueobj);
 }
 
-void SetJsonPF(int Registro, int Sample){
+void PF(int Registro, int Sample){
     int Temp = 0;
     float Valueobj = 0;
     
@@ -851,21 +826,21 @@ void Read_registers(int Sample)
     {
         if ( i == 56 || i == 59 || i == 62 || i == 65 || i == 66 || i == 127 || i == 129 || i == 131 || i == 118 || i == 120 || i == 122  )
         {
-            SetJsonCurrent(i, Sample);
+            Current(i, Sample);
         } else if ( i == 57 || i == 60 || i == 63 || i == 126 || i == 128 || i ==130 || i == 132 || i == 117 || i == 119 || i == 121 )
         {
-            SetJsonVol(i, Sample);
+            Vol(i, Sample);
         // } else if ( i == 72 || i == 73 || i == 74 || i == 75 || i == 76 || i == 77 || i == 78 || i == 79 || i == 80 || i == 81 || i == 82 || i == 83){
         } else if ( i >= 72 && i <= 86 || i >= 108 && i <= 116 ){
-            SetJsonPower( i, Sample );
+            Power( i, Sample );
         }
          else if ( i >= 117 && i <= 122 ){
-            SetJsonTHD( i, Sample );
+            THD( i, Sample );
         } 
         else if (  i >= 135 && i <= 137 ){
-            SetJsonAngle(i, Sample);
+            Angle(i, Sample);
         } else if ( i >= 165 && i <= 167 ){
-            SetJsonPF(i, Sample);
+            PF(i, Sample);
         }
         i++;
     }
