ab240efca791f526ee520398425aaef99e25926c
kevinfcc26 <kevinfcc26@gmail.com>
2020-06-19 17:25:04 -0500

    add test

--- mysql.cpp
+++ mysql.cpp
@@ -38,6 +38,7 @@ int main(){
     MYSQL_RES *res_set;
     MYSQL_ROW row;
     mysql_query (connect,"INSERT INTO test VALUES (1,kevin)");
+    mysql_query (connect,"SELCT*FROM test");
     unsigned int i =0;
     res_set = mysql_store_result(connect);
     unsigned int numrows = mysql_num_rows(res_set);
