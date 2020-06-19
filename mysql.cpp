549eb0fc2fd4adb616cc33fed34da3f25316cb15
kevinfcc26 <kevinfcc26@gmail.com>
2020-06-19 17:09:23 -0500

    add test

--- mysql.cpp
+++ mysql.cpp
@@ -21,21 +21,41 @@ int main(){
 	// char consulta[con.size() + 1];
 	// strcpy(consulta, con.c_str());
     //  cout<<consulta;
-     MYSQL *connect;
-     connect=mysql_init(NULL);
-     if (!connect){
-          cout<<"MySQL Initialization failed";
-          return 1;
-     }
-     connect=mysql_real_connect(connect, SERVER, USER, PASSWORD , DATABASE ,0,NULL,0);
-     if (connect){
-          cout<<"connection Succeeded\n";
-     }
-     else{
-          cout<<"connection failed\n";
-     }
+    MYSQL *connect;
+    connect=mysql_init(NULL);
+    if (!connect){
+        cout<<"MySQL Initialization failed";
+        return 1;
+    }
+    connect=mysql_real_connect(connect, SERVER, USER, PASSWORD , DATABASE ,0,NULL,0);
+    if (connect){
+        cout<<"connection Succeeded\n";
+    }
+    else{
+        cout<<"connection failed\n";
+    }
+
+    MYSQL_RES *res_set;
+    MYSQL_ROW row;
+    mysql_query (connect,"select * from test;");
+    unsigned int i =0;
+    res_set = mysql_store_result(connect);
+    unsigned int numrows = mysql_num_rows(res_set);
+
+    cout << endl;
+    cout <<"\t --------------------------------------------------------------------- \t"<< endl;
+
+    while (((row= mysql_fetch_row(res_set)) !=NULL ))
+    { //cout<<" %s\n",row[i] !=NULL?row[i] : "NULL";
+
+    cout <<"\t | \t" << row[i] << "\t | \t"<<  row[i+1] << "\t | \t"<< row[i+2] << "\t | \t" << endl;
+    cout <<"\t --------------------------------------------------------------------- \t"<< endl;
+    }
+
+    mysql_close (connect);
+    return 0;
      
     //  mysql_query(connect, consulta);
      // Cerrar la conexión
-     mysql_close(connect);     
+    //  mysql_close(connect);     
 }
