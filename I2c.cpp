e03b4f70302309a47b40c20c83764cb735155e06
kevinfcc26 <kevinfcc26@gmail.com>
2020-06-23 12:41:11 -0500

    banderas

--- I2c.cpp
+++ I2c.cpp
@@ -964,10 +964,13 @@ void Query(){
     int i = 0;
 
     for ( i= 0 ; i < 181; i ++){
-        if(i == 56 || i == 57 || i == 59 || i == 60 || i == 62 || i == 63 || i == 65 || i == 66 || i>=72 && i <= 86 || i >= 108 && i <= 123 || i >= 127 && i <= 132 || i >= 135 && i <= 137 || i >= 165 && i <= 167 || i >= 178 && i <= 180 ){
+        if(i == 56 || i == 57 || i == 59 || i == 60 || i == 62 || i == 63 || i == 65 || i == 66 || i>=72 && i <= 86 || i >= 108 && i <= 122 || i >= 127 && i <= 132 || i >= 135 && i <= 137 || i >= 165 && i <= 167 ){
         col = col + "'" + Objregister[i].GetName() + "'" + ",";
         values = values+ std::to_string(Objregister[i].GetConValue()) + "," ;
         }
+    }
+    for (   i = 0; i<= 35; i++ ){
+        c
     }
     insert = insert + col + " ) VALUES (" + values + ")";
     cout << insert << endl;
