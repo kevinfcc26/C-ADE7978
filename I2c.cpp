8c296d09d619c1dc6bd6f54afbea6a9589a883b1
kevinfcc26 <kevinfcc26@gmail.com>
2020-06-23 17:10:22 -0500

    time

--- I2c.cpp
+++ I2c.cpp
@@ -986,7 +986,7 @@ void Query(){
         if(isnan(RCal[i].get())){
             values = values + std::to_string( 0 ) + ",";
         }else {
-        values = values + std::to_string( RCal[i].get() ) + ",";
+        values = values + "`" + std::to_string( RCal[i].get() ) + "`,";
         }
     }
     insert = insert + col + "`DATETIME`" + " ) VALUES (" + values + '"' + t() + '"' + ");";
