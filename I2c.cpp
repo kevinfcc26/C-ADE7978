46b591097d9b528278e338c24d0822536dd61c71
kevinfcc26 <kevinfcc26@gmail.com>
2020-06-23 13:11:29 -0500

    time

--- I2c.cpp
+++ I2c.cpp
@@ -979,8 +979,16 @@ void Query(){
     cout << insert << endl;
 }
 string time(){
-    time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    return std::to_string( now );
+    time_t current_time;
+    struct tm * time_info;
+    char timeString[9];
+
+    time(&current_time);
+    time_info = localtime(&current_time);
+
+    strftime(timeString, sizeof(timeString), "%H:%M:%S", time_info);
+    puts(timeString);
+    return 0;
 }
 // filtro para cargar los registros que se van a enviar
 void Read_registers(int Sample)
