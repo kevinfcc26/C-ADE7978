c35ba03cdcdb2dbb9ad8f4cb4a278bdb98be01e9
kevinfcc26 <kevinfcc26@gmail.com>
2020-07-11 21:10:14 -0500

    delate +1 in pf

--- I2c.cpp
+++ I2c.cpp
@@ -913,7 +913,7 @@ void PF(int Registro, int Sample){
     Objregister[Registro].Read();
     Temp = Objregister[Registro].GetValue();
     if( (Temp  && 0x8000) == 0x8000 ){
-        Temp = not( Temp + 1 );
+        Temp = not( Temp );
     } else{
         Temp = Temp* pow(2,-15); 
     }
